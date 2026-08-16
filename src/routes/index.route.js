b4f29946e3ab61c0efc2265f7d88619737903a85
Rupesh09876 <rupeshkatuwal53@gmail.com>
2026-08-16 11:58:36 +0545

    routes: fixing the error

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,17 @@
 import express from "express";
 import authRouter from "../modules/auth/auth.route.js";
 import planRouter from "../modules/plan/plan.route.js";
+import memberRoute from "../modules/member/member.route.js";
+
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/plan", planRouter);
+router.use("/plan", planRouter);
+router.use("/member", memberRoute);
+
+
+
 
 export default router;
