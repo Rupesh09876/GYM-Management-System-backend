7117d4154b15e0362dbe6c3cd2d1f2379aeeb3ca
Rupesh09876 <rupeshkatuwal53@gmail.com>
2026-09-08 14:46:29 +0545

    feat: update API routes for member management operations

--- src/modules/member/member.route.js
+++ src/modules/member/member.route.js
@@ -1,23 +1,23 @@
 import express from "express";
-
+import { authenticateUser, adminMiddleware } from "../../middlewares/authMiddleware.js";
 import {
-    createMemberController, getAllMembersController, getMemberByIdController, updateMemberController, blockMemberController, unblockMemberController           } from "./member.controller.js";
-
+    createMemberController,
+    getAllMembersController,
+    getMemberByIdController,
+    updateMemberController,
+    blockMemberController,
+    unblockMemberController
+} from "./member.controller.js";
 
 const router = express.Router();
 
+router.use(authenticateUser, adminMiddleware);
 
 router.post("/create", createMemberController);
-
 router.get("/", getAllMembersController);
-
 router.get("/:id", getMemberByIdController);
-
 router.put("/:id", updateMemberController);
-
 router.patch("/:id/block", blockMemberController);
-
 router.patch("/:id/unblock", unblockMemberController);
-
 
 export default router;
