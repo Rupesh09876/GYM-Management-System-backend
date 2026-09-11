e1106174d0587b0ae716a977b3a5e1e8fd125a30
Rupesh09876 <rupeshkatuwal53@gmail.com>
2026-09-11 12:23:38 +0545

    feat: define API routes for payment management and transactions

--- src/modules/payments/payments.route.js
+++ src/modules/payments/payments.route.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { authenticateUser, adminMiddleware } from "../../middlewares/authMiddleware.js";
-import { getPaymentsDataController, createPaymentController, initiateKhaltiController, verifyKhaltiController } from "./payments.controller.js";
+import {
+    getPaymentsDataController,
+    createPaymentController,
+    initiateKhaltiController,
+    verifyKhaltiController,
+    downloadInvoiceController
+} from "./payments.controller.js";
 
 const router = express.Router();
 
 router.use(authenticateUser);
 
 router.get("/", getPaymentsDataController);
+router.get("/invoice/download", downloadInvoiceController);
+router.get("/invoice/download/:id", downloadInvoiceController);
 router.post("/", adminMiddleware, createPaymentController);
 router.post("/create", adminMiddleware, createPaymentController);
 
@@ -14,5 +22,3 @@ router.post("/khalti/initiate", initiateKhaltiController);
 router.post("/khalti/verify", verifyKhaltiController);
 
 export default router;
-
-
